1776baa234caef44db8053db4c60d8a2a58a7604
Christina Polyukh <christinapolyukh@Christinas-MacBook-Air.local>
2022-08-24 11:31:09 -0400

    File structure updates

--- Contracts/contracts/CircleNFT.sol
+++ Contracts/contracts/CircleNFT.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract CircleNFT is ERC721, ERC721URIStorage, Ownable {
     constructor() ERC721("ChristinaNFT", "CNFT") {}
 
---
+++ Contracts/scripts/deploy-nft.ts
@@ -0,0 +1,8 @@
+import { ethers } from "ethers";
+import "dotenv/config"
+// import * as CircleNFT from "../a"
+
+// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
+// Do never expose your keys like this
+const EXPOSED_KEY =
+  "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
