27b5cb0ae5a68e8f5551bdac0a4332949a78a83b
Alice He <alice041189@gmail.com>
2022-08-30 12:45:27 -0700

    use eth in escrow

--- Contracts/contracts/Escrow.sol
+++ Contracts/contracts/Escrow.sol
@@ -2,37 +2,36 @@
 pragma solidity ^0.8.0;
 
 import "./IERC20.sol";
-import "./ILendingPool.sol";
+import "./IWETHGateway.sol";
 
 contract Escrow {
     address arbiter;
     address depositor;
     address beneficiary;
-    uint256 initialDeposit;
+    uint initialDeposit;
+    IWETHGateway gateway = IWETHGateway(0xDcD33426BA191383f1c9B431A342498fdac73488);
+    IERC20 aWETH = IERC20(0x030bA81f1c18d280636F32af80b9AAd02Cf0854e);
 
-    // the mainnet AAVE v2 lending pool
-    ILendingPool pool = ILendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
-    // aave interest bearing DAI
-    IERC20 aDai = IERC20(0x028171bCA77440897B824Ca71D1c56caC55b68A3);
-    // the DAI stablecoin 
-    IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-    constructor(address _arbiter, address _beneficiary, uint256 _amount) payable {
+    constructor(address _arbiter, address _beneficiary) payable {
         arbiter = _arbiter;
         beneficiary = _beneficiary;
         depositor = msg.sender;
-        initialDeposit = _amount; 
-    
-        dai.transferFrom(msg.sender, address(this), _amount);
-        dai.approve(address(pool), _amount);
+        initialDeposit = msg.value;
 
-        pool.deposit(address(dai), _amount, address(this), 0);
+        gateway.depositETH{value: address(this).balance}(address(this), 0);
     }
+
+    receive() external payable {}
 
     function approve() external {
         require(msg.sender == arbiter);
+
+        uint balance = aWETH.balanceOf(address(this));
+        aWETH.approve(address(gateway), balance);
+
+        gateway.withdrawETH(balance, address(this));
+        payable(beneficiary).transfer(initialDeposit);
         
-        pool.withdraw(address(dai), initialDeposit, beneficiary);
-        
-        pool.withdraw(address(dai), type(uint).max, beneficiary);
+        selfdestruct(payable(depositor));
     }
 }
