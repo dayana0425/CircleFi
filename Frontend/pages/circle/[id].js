57b5c340b320d074f9016cc32407cc0727a3eb1d
kevinledev <kevinle.developer@gmail.com>
2022-08-31 15:46:28 -0700

    changes to [id] page

--- Frontend/pages/circle/[id].js
+++ Frontend/pages/circle/[id].js
@@ -13,6 +13,9 @@ import {
   EmojiHappyIcon,
   TicketIcon,
   UsersIcon,
+  LinkIcon,
+  XCircleIcon,
+  CheckCircleIcon,
 } from "@heroicons/react/outline";
 
 function checkIfAlreadyRegistered() {
@@ -51,7 +54,9 @@ function Event({ event }) {
         setLoading(false);
         setMessage("You successfully registered!");
       } else {
-        console.log("Error calling contract function: registerToSavingCircle().");
+        console.log(
+          "Error calling contract function: registerToSavingCircle()."
+        );
       }
     } catch (error) {
       setSuccess(false);
@@ -104,7 +109,9 @@ function Event({ event }) {
         setLoading(false);
         setMessage("You successfully started the first round!");
       } else {
-        console.log("Error getting calling contract function: startFirstRound()");
+        console.log(
+          "Error getting calling contract function: startFirstRound()"
+        );
       }
     } catch (error) {
       setSuccess(false);
@@ -129,7 +136,9 @@ function Event({ event }) {
         setLoading(false);
         setMessage("You successfully ended the round & started the next one!");
       } else {
-        console.log("Error getting calling contract function: startFirstRound()");
+        console.log(
+          "Error getting calling contract function: startFirstRound()"
+        );
       }
     } catch (error) {
       setSuccess(false);
@@ -154,7 +163,9 @@ function Event({ event }) {
         setLoading(false);
         setMessage("Circle Completed!");
       } else {
-        console.log("Error getting calling contract function: completeCircle()");
+        console.log(
+          "Error getting calling contract function: completeCircle()"
+        );
       }
     } catch (error) {
       setSuccess(false);
@@ -179,7 +190,9 @@ function Event({ event }) {
         setLoading(false);
         setMessage("Deadline successfully extended.");
       } else {
-        console.log("Error getting calling contract function: extendDeadline()");
+        console.log(
+          "Error getting calling contract function: extendDeadline()"
+        );
       }
     } catch (error) {
       setSuccess(false);
@@ -202,9 +215,13 @@ function Event({ event }) {
         console.log("Minted -- ", txn.hash);
         setSuccess(true);
         setLoading(false);
-        setMessage("Emergency withdrawal successfully completed. Everyone recieved their funds back.");
+        setMessage(
+          "Emergency withdrawal successfully completed. Everyone recieved their funds back."
+        );
       } else {
-        console.log("Error getting calling contract function: emergencyWithdrawal()");
+        console.log(
+          "Error getting calling contract function: emergencyWithdrawal()"
+        );
       }
     } catch (error) {
       setSuccess(false);
@@ -212,6 +229,68 @@ function Event({ event }) {
       setLoading(false);
       console.log(error);
     }
+  };
+
+  const hostControls = () => {
+    return (
+      <div>
+        <div className="flex item-center">
+          <span className="truncate">
+            <button
+              type="button"
+              className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-green-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              onClick={startFirstRound}
+            >
+              Start First Round
+            </button>
+          </span>
+        </div>
+        <div className="flex item-center">
+          <span className="truncate">
+            <button
+              type="button"
+              className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-green-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              onClick={endRoundAndStartNextRound}
+            >
+              End Round
+            </button>
+          </span>
+        </div>
+        <div className="flex item-center">
+          <span className="truncate">
+            <button
+              type="button"
+              className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-green-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              onClick={endRoundAndStartNextRound}
+            >
+              Complete Saving Circle
+            </button>
+          </span>
+        </div>
+        <div className="flex item-center">
+          <span className="truncate">
+            <button
+              type="button"
+              className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-pink-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              onClick={extendDeadline}
+            >
+              Extend Deadline For Round
+            </button>
+          </span>
+        </div>
+        <div className="flex item-center">
+          <span className="truncate">
+            <button
+              type="button"
+              className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-pink-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              onClick={emergencyWithdrawal}
+            >
+              Emergency Withdrawl
+            </button>
+          </span>
+        </div>
+      </div>
+    );
   };
 
   return (
@@ -246,29 +325,46 @@ function Event({ event }) {
             color={"palevioletred"}
           />
         )}
-        <h6 className="mb-2">{formatTimestamp(event.eventTimestamp)}</h6>
-        <h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl mb-6 lg:mb-12">
+        {/* <h6 className="mb-2">{formatTimestamp(event.eventTimestamp)}</h6> */}
+        <div className="flex flex-row items-center font-thick text-xl">          
+        {event.imageURL && (
+          <Image
+            className="rounded-full"
+            src={event.imageURL}
+            alt="event image"
+            width="100px"
+            height="100px"
+          />
+        )}
+        <h1 className="text-4xl tracking-tight">
           {event.circleName}
         </h1>
+        </div>
         <div className="flex flex-wrap-reverse lg:flex-nowrap">
           <div className="w-full pr-0 lg:pr-24 xl:pr-32">
-            <div className="mb-8 w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-              {event.imageURL && (
-                <Image src={event.imageURL} alt="event image" layout="fill" />
-              )}
-            </div>
+            {/* <div className="mb-8 w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden "> */}
+            {/* </div> */}
             <p>{event.description}</p>
           </div>
           <div className="max-w-xs w-full flex flex-col gap-4 mb-6 lg:mb-0">
             <div className="flex item-center">
-              <UsersIcon className="w-6 mr-2" />
+              {parseInt(event.round) === 0 ? (
+                <XCircleIcon className="w-6 mr-2" />
+              ) : (
+                <CheckCircleIcon className="w-6 mr-2" />
+              )}
+
               <span className="truncate">
-                {event.totalRSVPs}/{event.maxCapacity} attending
+                {parseInt(event.round) === 0
+                  ? "Round 0 - Circle starting soon"
+                  : `Round ${event.round}`}
               </span>
             </div>
             <div className="flex item-center">
-              <TicketIcon className="w-6 mr-2" />
-              <span className="truncate">1 RSVP per wallet</span>
+              <UsersIcon className="w-6 mr-2" />
+              <span className="truncate">
+                {event.participantCounter} / {event.groupSize} attending
+              </span>
             </div>
             <div className="flex items-center">
               <EmojiHappyIcon className="w-10 mr-2" />
@@ -278,106 +374,110 @@ function Event({ event }) {
                   className="text-indigo-800 truncate hover:underline"
                   href={`${process.env.NEXT_PUBLIC_TESTNET_EXPLORER_URL}address/${event.eventOwner}`}
                   target="_blank"
-                  rel="noreferrer">
+                  rel="noreferrer"
+                >
                   {event.host}
                 </a>
               </span>
             </div>
             <div className="flex item-center">
-              <span className="truncate">     
-              { account ? (     
-                <button
-                  type="button"
-                  className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  onClick={registerToSavingCircle}>
-                  Register for {ethers.utils.formatEther(event.saveAmount)} ETH
-                </button> ) : ( <ConnectButton /> )
-              }
+              <span className="truncate">
+                {account ? (
+                  <button
+                    type="button"
+                    className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={registerToSavingCircle}
+                  >
+                    Register for {ethers.utils.formatEther(event.saveAmount)}{" "}
+                    ETH
+                  </button>
+                ) : (
+                  <ConnectButton />
+                )}
               </span>
             </div>
             <div className="flex item-center">
-              <span className="truncate">     
-              { account ? (     
-                <button
-                  type="button"
-                  className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  onClick={makePayment}>
-                  Make Payment
-                </button> ) : ( "Make Payments Once Round Begins")
-              }
+              <span className="truncate">
+                {account ? (
+                  <button
+                    type="button"
+                    className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={makePayment}
+                  >
+                    Make Payment
+                  </button>
+                ) : (
+                  "Make Payments Once Round Begins"
+                )}
               </span>
             </div>
-            <div className="flex item-center">
-              <span className="truncate">     
-              { (account) ? 
-                (account.address.toString().toLowerCase().trim() === event.host.toString().trim()) ?      
-                <button
-                  type="button"
-                  className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-green-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                  onClick={startFirstRound}>
-                  Start First Round
-                </button>
-                : ("") : ("")
-              }
-              </span>
-            </div>
-            <div className="flex item-center">
-              <span className="truncate">     
-              { (account) ? 
-                (account.address.toString().toLowerCase().trim() === event.host.toString().trim()) ?      
-                <button
-                  type="button"
-                  className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-green-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                  onClick={endRoundAndStartNextRound}>
-                  End Round
-                </button>
-                : ("") : ("")
-              }
-              </span>
-            </div>
-            <div className="flex item-center">
-              <span className="truncate">     
-              { (account) ? 
-                (account.address.toString().toLowerCase().trim() === event.host.toString().trim()) ?      
-                <button
-                  type="button"
-                  className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-green-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                  onClick={endRoundAndStartNextRound}>
-                  Complete Saving Circle
-                </button>
-                : ("") : ("")
-              }
-              </span>
-            </div>
-            <div className="flex item-center">
-              <span className="truncate">     
-              { (account) ? 
-                (account.address.toString().toLowerCase().trim() === event.host.toString().trim()) ?      
-                <button
-                  type="button"
-                  className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-pink-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                  onClick={extendDeadline}>
-                  Extend Deadline For Round
-                </button>
-                : ("") : ("")
-              }
-              </span>
-            </div>
-            <div className="flex item-center">
-              <span className="truncate">     
-              { (account) ? 
-                (account.address.toString().toLowerCase().trim() === event.host.toString().trim()) ?      
-                <button
-                  type="button"
-                  className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-pink-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                  onClick={emergencyWithdrawal}>
-                  Emergency Withdrawl
-                </button>
-                : ("") : ("")
-              }
-              </span>
-            </div>
-
+            {account ? (
+              account.address.toString().toLowerCase().trim() ===
+              event.host.toString().trim() ? (
+                <div className="flex flex-col gap-4 border border-2 border-indigo-200 rounded-md p-4">
+                  <h1 className="text-indigo-700">Host Controls</h1>
+                  <div className="flex item-center">
+                    <span className="truncate">
+                      <button
+                        type="button"
+                        className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-green-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        onClick={startFirstRound}
+                      >
+                        Start First Round
+                      </button>
+                    </span>
+                  </div>
+                  <div className="flex item-center">
+                    <span className="truncate">
+                      <button
+                        type="button"
+                        className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-green-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        onClick={endRoundAndStartNextRound}
+                      >
+                        End Round
+                      </button>
+                    </span>
+                  </div>
+                  <div className="flex item-center">
+                    <span className="truncate">
+                      <button
+                        type="button"
+                        className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-green-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        onClick={endRoundAndStartNextRound}
+                      >
+                        Complete Saving Circle
+                      </button>
+                    </span>
+                  </div>
+                  <div className="flex item-center">
+                    <span className="truncate">
+                      <button
+                        type="button"
+                        className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-pink-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        onClick={extendDeadline}
+                      >
+                        Extend Deadline For Round
+                      </button>
+                    </span>
+                  </div>
+                  <div className="flex item-center">
+                    <span className="truncate">
+                      <button
+                        type="button"
+                        className="w-full items-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-indigo-700 bg-pink-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        onClick={emergencyWithdrawal}
+                      >
+                        Emergency Withdrawl
+                      </button>
+                    </span>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </section>
@@ -396,6 +496,9 @@ export async function getServerSideProps(context) {
           id
           circleName
           frequency
+          groupSize
+          participantCounter
+          round
           host
           imageURL
           description
@@ -411,7 +514,7 @@ export async function getServerSideProps(context) {
   return {
     props: {
       event: data.savingCircle,
-    }
+    },
   };
 }
 
